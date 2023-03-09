6dbfa6625a81f68f129e4bb155a68da8fa3918f5
matt zhou <matt@cruelyouth.com>
2023-03-09 22:34:40 +0800

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from 'react'
 interface AppProps {}
 
 const App: FunctionComponent<AppProps> = () => {
-  return <h1>Hello world!</h1>
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
 export default App
