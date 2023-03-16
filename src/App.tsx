763ffb395f3b4a7965e9af56df9b667675546af2
matt zhou <matt@cruelyouth.com>
2023-03-16 15:49:25 +0800

    text drop down

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ import VerseDropDown from './components-ui/BibleSelector/VerseDropDown'
 import Modal from './components-ui/Modal'
 import Sidebar from './components-ui/Sidebar'
 import Tabs from './components-ui/Tabs'
+import TextInputDropDown from './components-ui/TextInputDropDown'
 import VersionDropDown from './components-ui/VersionDropDown'
 interface AppProps {}
 
 const App: FunctionComponent<AppProps> = () => {
   return (
     <div className="tw-w-screen tw-h-screen">
-      <Modal />
+      <TextInputDropDown />
     </div>
   )
 }
